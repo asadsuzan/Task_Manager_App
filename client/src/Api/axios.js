a34697ef4c923e6547787ea1f60c03d2bcb659d7
suzan <asadsuzan7@gmail.com>
2023-01-08 21:11:04 +0600

    design the tasks card in front end

--- client/src/Api/axios.js
+++ client/src/Api/axios.js
@@ -72,3 +72,7 @@ export const reqToSaveTask = (task) => {
   })
 
 }
+
+// export const reqToGetTaskByCate=()=>{
+   
+// }
--- client/src/components/Inprogress.jsx
+++ client/src/components/Inprogress.jsx
@@ -1,9 +1,44 @@
 import React from 'react'
+import { MdRadioButtonUnchecked } from 'react-icons/md'
+import { AiOutlineStar } from 'react-icons/ai'
+import { TbTrash } from 'react-icons/tb'
 
 const Inprogress = () => {
   return (
-    <div>
-      Inprogress
+    <div className='task-card p-3'>
+      <div className="task-content">
+        <h5>todo application</h5>
+        <p>complete the all routes for app just today </p>
+      </div>
+      <div className="task-actions d-flex align-items-center justify-content-between">
+        <div className="left-side d-flex align-items-center">
+          <div className="due-date">
+            <input type="date" />
+            <span>24-02-2023</span>
+          </div>
+          <div className="category">
+
+            <select id='category' required>
+              <option value="none">Task</option>
+              <option value="my-day">My Day</option>
+              <option value="important">Important</option>
+              <option value="inprogress">Inprogress</option>
+            </select>
+            <span>Important</span>
+          </div>
+        </div>
+        <div className="right-side d-flex align-items-center">
+          <div className="isCompleted">
+            <MdRadioButtonUnchecked size={'20px'} color="green" />
+          </div>
+          <div className='isImportant'>
+            <AiOutlineStar size={'20px'} color="orange" />
+          </div>
+          <div className='delete'>
+            <TbTrash size={'20px'} color="red" />
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
